06baa6923ec1634fa35ac16e5f7fa23147b74ad9
Jewls Krueger <Juliarkrueger@gmail.com>
2019-12-23 12:24:22 -0800

    setting up heroku

--- clients/clients/settings.py
+++ clients/clients/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(-q=1+ow5u0r5#g9%w$^6xdla!7^s85x$6+)3y@6pfc_f8v8ll'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sales-heroku-jewls.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
@@ -72,14 +74,9 @@ WSGI_APPLICATION = 'clients.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 # Password validation
@@ -131,5 +128,7 @@ MEDIA_ROOT = 'mymedia'
 LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = 'client_list'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #CRUD = Create Read Update Delete
